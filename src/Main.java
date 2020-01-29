a25fa1c2342c7b23d703e75cb743126620d2d628
nafiseh <fun101.nm@gmail.com>
2020-01-29 13:16:03 +0330

    homwor0-2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by admin on 9/20/2019.
+ */
+public class Main {
+    public Main() {
+    }
+    public static void main(String[] args)
+    {
+
+        if(true)
+        {
+            System.out.print("hellow");
+        }else
+        {
+            System.out.print("goodbye");
+        }
+
+    }
+}
